60b7e61b4b59d3cd69f211f98fb637925196f4ad
Tiago Sobral <113380453+TiagoSobral@users.noreply.github.com>
2025-06-28 19:15:29 +0100

    (feat) add toDoList class constructor & projects array.

---
+++ src/javascript/to-do-list.js
@@ -0,0 +1,14 @@
+let projects = [];
+
+
+export class toDoList{
+    checkList = [];
+
+    constructor(title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+
+}
