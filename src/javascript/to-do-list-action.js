94e26ae4e246808b4927d1e8a48e25d876cff94d
Tiago Sobral <113380453+TiagoSobral@users.noreply.github.com>
2025-06-28 19:19:12 +0100

    (feat) add & remove from checklist inside toDo

---
+++ src/javascript/to-do-list-action.js
@@ -0,0 +1,41 @@
+
+import { projects, toDoList } from "./list.js";
+
+
+export function addToDoList(title, description, dueDate, priority){
+    const newToDo = new toDoList(title, description, dueDate, priority);
+
+    projects.push(newToDo);
+    console.log(projects);
+}
+
+export function addItemToCheckList(toDoListName, itemCheckList) {
+
+    for (let toDo of projects) {
+        
+        if (toDo.title === toDoListName) {
+            
+            toDo.checkList.push(itemCheckList);
+        }
+    }
+}
+
+export function removeItemFromCheckList (toDoListName, itemCheckList) {
+    
+    for (let toDo of projects) {
+        
+        let checklist = toDo.checkList;
+        
+        for (let item of checklist) {
+            
+            if (toDo.title === toDoListName && item === itemCheckList) {
+                
+                let itemIndex = checklist.indexOf(item);
+
+                checklist.splice(itemIndex,1);
+
+            }
+        }
+    }
+}
+
