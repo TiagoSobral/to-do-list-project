a0142274a9133f4775a8d3b610909014ddff064e
Tiago Sobral <113380453+TiagoSobral@users.noreply.github.com>
2025-06-30 16:42:42 +0100

    (feat) add find & changeCompleteStatus functions.

--- src/javascript/to-do-list-action.js
+++ src/javascript/to-do-list-action.js
@@ -3,6 +3,7 @@ import { allProjects, Project, ToDoItem } from "./to-do-list.js";
 
 
 export function createToDoList(name) {
+   
     const newToDoList = new Project(name);
     
     allProjects.push(newToDoList);
@@ -11,11 +12,11 @@ export function createToDoList(name) {
 
 
 export function addToDoItem(selectProject, title, description, dueDate, priority){
+    
     const newToDo = new ToDoItem(title, description, dueDate, priority);
 
-    let groupOfProjects = allProjects;
 
-    groupOfProjects.map((project) => { 
+    allProjects.map((project) => { 
         if (project.projectName === selectProject) {
             project.toDoArray.push(newToDo);
         }
@@ -24,16 +25,49 @@ export function addToDoItem(selectProject, title, description, dueDate, priority
 
 }
 
-export function changeStatus(bolean, selectProject, selectToDo) {
+export function changeCompletionStatus(answer, selectProject, selectToDo) {
     
     for (let project of allProjects) {
         
-        for (let toDo of project.toDoArray) {
+        for (let toDoItem of project.toDoArray) {
             
-            if(project.projectName === selectProject && toDo.title === selectToDo) {
+            if(project.projectName === selectProject && toDoItem.title === selectToDo) {
                 
-                toDo.taskStatus(bolean);
+                toDoItem.taskStatus(answer);
             }
         }
 }
+}
+
+function find(selectProject, selectToDo) {
+
+    let projectIndex;
+    
+    let toDoIndex;
+    
+    for (let project of allProjects) {
+        
+        for (let toDoItem of project.toDoArray) {
+            
+            if (project.projectName === selectProject && toDoItem.title === selectToDo) {
+               
+                projectIndex = allProjects.indexOf(project);
+                
+                toDoIndex = project.toDoArray.indexOf(toDoItem);
+            }
+        }
+    }
+    return {projectIndex, toDoIndex};
+}
+
+export function removeToDo(selectProject, selectToDo) {
+    
+    const bring = find(selectProject, selectToDo);
+
+    let chosenProject = allProjects[bring.projectIndex];
+
+    let toDosList = chosenProject.toDoArray;
+
+    toDosList.splice(bring.toDoIndex,1);
+
 }
