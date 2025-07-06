64576283383c5131225bdeabe2bc4e18324aedbc
Tiago Sobral <113380453+TiagoSobral@users.noreply.github.com>
2025-07-06 23:24:13 +0100

    (fix) changes imports calls new initial functions.

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -4,23 +4,21 @@ import "../css/active-styling.css"
 
 import { createToDoList, addToDoItem, changeCompletionStatus, removeToDo} from "./to-do-list-action"
 import { createDOM } from "./DOM-creation.js";
-import { addProjectListener, addTaskListener, listenerProjectExpand } from "./DOM-logic.js";
+import { addProjectListener, addTaskListener, listenerProjectExpand , showToDosFromFilter} from "./DOM-logic.js";
 import { allProjects } from "./to-do-list.js";
 
 createToDoList();
 createToDoList("Family");
 createToDoList("Travel");
 
-addToDoItem("Cleaning", "Weekly Chores", "2022-02-22",3, "Family");
-addToDoItem("zapp", "Weekly Chores", "2022-01-12", 3, "Family");
-addToDoItem("Packing", "Weekly Chores", "2022-12-22", 5, "Travel");
-addToDoItem("Finish Project", "To Do List", "2025-07-08", 5);
+addToDoItem("Cleaning", "Weekly Chores", "2022-02-22", "High", "Family");
+addToDoItem("zapp", "Weekly Chores", "2022-01-12", "Low", "Family");
+addToDoItem("Packing", "Weekly Chores", "2022-12-22", "Medium", "Travel");
+addToDoItem("Finish Project", "To Do List", "2025-07-07", "High");
 
 
 createDOM();
 listenerProjectExpand();
-addProjectListener();
-addTaskListener();
 
 console.log(allProjects);
 
