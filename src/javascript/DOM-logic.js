b450e245000f90d279504137b039b69543481665
Tiago Sobral <113380453+TiagoSobral@users.noreply.github.com>
2025-07-09 00:28:24 +0100

    (feat) add editToDo & add getPreviousPriorityValue
    -> editToDo has a eventListener to the edit icon.
    
    -> removes that element line.
    
    -> it will then take all the values from that Lines
    and use them to call addInputsForToDos.
    
    -> getPreviousPriorityValue is called to fetch the
    value of the option selected.
    
    (feat) add toDosByFilter.
    
    -> this function brings all the toDos from gatherToDos;
    
    -> sets the date of today with a format from date-fns
    
    -> checks wether the filter name is "Upcoming",
    "Today", or "Complete". Based on that it iterates
    through  the gatherToDos & filters based on the condition.
    
    -> finally it calls showFilteredToDos with the
    filtered array as a parameter.

--- src/javascript/DOM-logic.js
+++ src/javascript/DOM-logic.js
@@ -1,8 +1,10 @@
-import { arrayProjectsToDOM, showProject } from "./DOM-content";
-import { addInputForNameProject, addInputsForToDos, btnAddToDo, removeProjectContentDOM, removeProjectListDOM } from "./DOM-creation";
-import { addToDoItem, changeCompletionStatus, createToDoList, removeProject, removeToDo } from "./to-do-list-action";
+import { format } from "date-fns";
+import { arrayProjectsToDOM, showFilteredToDos, showProject } from "./DOM-content";
+import { addInputForNameProject, addInputsForToDos, btnAddToDo, removeProjectContentDOM, removeProjectListDOM, toDoElements } from "./DOM-creation";
+import { addToDoItem, changeCompletionStatus, createToDoList, gatherToDos, removeProject, removeToDo } from "./to-do-list-action";
  
 
+        // LEFT SIDE
 
 export function listenerProjectExpand() {
 
@@ -42,6 +44,28 @@ export function addProjectListener() {
 }
 
 
+export function filterListener() {
+
+    const btnsFilter = document.querySelectorAll(".today, .upcoming, .completed-todos");
+
+    btnsFilter.forEach((btn) => {
+
+            btn.addEventListener("click", () => {
+
+                // debugger;
+            let btnValue = btn.textContent;
+        
+            removeProjectContentDOM();
+
+            toDosByFilter(btnValue);
+
+        })
+
+});
+
+}
+
+
 function inputListener() {
     
     const inputName = document.querySelector("input");
@@ -61,6 +85,178 @@ function inputListener() {
         }
 
     } )
+
+}
+
+
+export function delProjectListener() {
+
+    const trashIcons = document.querySelectorAll(".del-project");
+
+    trashIcons.forEach((trash) => {
+
+        trash.addEventListener("click", () => {
+
+            const parentNode = trash.parentElement;
+
+            const parentNodeList = parentNode.nextElementSibling;
+
+        parentNode.remove();
+        parentNodeList.remove();
+
+        removeProject(trash.dataset.indexnumber);
+
+        });
+
+    })
+}
+
+function toDosByFilter(projectKey) {
+    // debugger;
+
+    const toDos = gatherToDos().arrayOfGathered;
+
+    const btnToday = document.querySelector(".today");
+
+    const btnTitle = btnToday.querySelector("span");
+
+    const projectTitle = document.querySelector(".project-title");
+
+    let filterBy;
+
+    let todaysDate = format(Date(), "yyyy-MM-dd");
+
+
+    if (projectKey === "Today") {
+
+       filterBy = toDos.filter((task) => task.dueDate == `${todaysDate}`);
+
+    }
+
+    else if (projectKey === "Upcoming") {
+
+       filterBy = toDos.filter((task) => task.dueDate > `${todaysDate}`);
+    }
+
+    else {
+        
+        filterBy = toDos.filter((task) => task.completion === "Yes");
+   
+    }
+
+    showFilteredToDos(filterBy);
+
+    projectTitle.textContent = projectKey;
+
+}
+
+
+        // RIGHT SIDE
+
+
+export function editToDo() {
+
+    const btnEdit = document.querySelectorAll(".edit-todo");
+
+
+    btnEdit.forEach((btn) => btn.addEventListener("click", () => {
+
+        // debugger;
+        const editParentElement = btn.parentElement;
+
+        const dataFromToDo = editParentElement.querySelectorAll("[data-class]");
+
+        let title = dataFromToDo[0].textContent;
+        let description = dataFromToDo[1].textContent;
+        let dueDate = dataFromToDo[2].textContent;
+        let priority = dataFromToDo[3].textContent;
+
+        
+        editParentElement.remove();
+
+        addInputsForToDos(title, description, dueDate);
+
+        getPreviousPriorityValue(priority);
+
+        btnAddListener();
+    
+        
+    }
+))
+
+};
+
+
+function getPreviousPriorityValue(previousOptionValue) {
+
+        const options = document.querySelectorAll("option");
+
+        options.forEach((option) => {
+
+            if (option.value === previousOptionValue) {
+                option.setAttribute("selected", "");
+            }
+
+        })
+
+};
+
+
+
+export function btnCompleteListener() {
+
+    const checkboxes = document.querySelectorAll(".checkbox");
+
+    checkboxes.forEach((checkbox) => {
+        
+        checkbox.addEventListener("click", () => {
+
+            const projectName = checkbox.parentElement.dataset.projectName;
+            const todoName = checkbox.parentElement.dataset.todoName;
+
+            const completeStatus = checkbox.previousElementSibling;
+            const status =  completeStatus.textContent;
+
+        if (status === "No") {
+
+            changeCompletionStatus("Yes", projectName, todoName);
+            completeStatus.textContent = "Yes";
+
+        }
+        else {
+            changeCompletionStatus("No", projectName, todoName);
+            completeStatus.textContent = "No";
+        }
+
+
+    })
+})
+};
+
+
+export function trashListener() {
+
+
+    const trash = document.querySelectorAll(".delete-to-do");
+
+    trash.forEach((trashIcon) => {
+
+
+        const projectTitle = trashIcon.parentElement.dataset.projectName;
+        const toDoName = trashIcon.parentElement.dataset.todoName;
+
+        const lineToErase = trashIcon.parentElement;
+
+        trashIcon.addEventListener("click", () => {
+
+            removeToDo(toDoName, projectTitle);
+            lineToErase.remove();
+
+
+
+        })
+
+    })
 
 }
 
@@ -93,11 +289,10 @@ function btnAddListener() {
         const todoSelect = document.querySelector("select");
 
 
-
         let inputTitle = todoInputs[0].value;
         let inputDescription = todoInputs[1].value;
         let inputDueDate = todoInputs[2].value;
-        let inputPriority = todoSelect[0].value;
+        let inputPriority = todoSelect.value;
 
         e.preventDefault();
 
@@ -115,81 +310,3 @@ function btnAddListener() {
 
 }
 
-
-export function trashListener() {
-
-
-    const trash = document.querySelectorAll(".delete-to-do");
-
-    trash.forEach((trashIcon) => {
-
-
-        const projectTitle = trashIcon.parentElement.dataset.projectName;
-        const toDoName = trashIcon.parentElement.dataset.todoName;
-
-        const lineToErase = trashIcon.parentElement;
-
-        trashIcon.addEventListener("click", () => {
-
-            removeToDo(toDoName, projectTitle);
-            lineToErase.remove();
-
-
-
-        })
-
-    })
-
-}
-
-export function delProjectListener() {
-
-    const trashIcons = document.querySelectorAll(".del-project");
-
-    trashIcons.forEach((trash) => {
-
-        trash.addEventListener("click", () => {
-
-            const parentNode = trash.parentElement;
-
-            const parentNodeList = parentNode.nextElementSibling;
-
-        parentNode.remove();
-        parentNodeList.remove();
-
-        removeProject(trash.dataset.indexnumber);
-
-        });
-
-    })
-}
-
-export function btnCompleteListener() {
-
-    const checkboxes = document.querySelectorAll(".checkbox");
-
-    checkboxes.forEach((checkbox) => {
-        
-        checkbox.addEventListener("click", () => {
-
-            const projectName = checkbox.parentElement.dataset.projectName;
-            const todoName = checkbox.parentElement.dataset.todoName;
-
-            const completeStatus = checkbox.previousElementSibling;
-            const status =  completeStatus.textContent;
-
-        if (status === "No") {
-
-            changeCompletionStatus("Yes", projectName, todoName);
-            completeStatus.textContent = "Yes";
-
-        }
-        else {
-            changeCompletionStatus("No", projectName, todoName);
-            completeStatus.textContent = "No";
-        }
-
-
-    })
-})
-}
