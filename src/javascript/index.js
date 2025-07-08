c67bfc71f74e9f84364b2d13746120f7a91a0501
Tiago Sobral <113380453+TiagoSobral@users.noreply.github.com>
2025-07-09 00:42:59 +0100

    (fix) changed the called functions to debug better

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -12,8 +12,8 @@ createToDoList("Family");
 createToDoList("Travel");
 
 addToDoItem("Cleaning", "Weekly Chores", "2022-02-22", "High", "Family");
-addToDoItem("zapp", "Weekly Chores", "2022-01-12", "Low", "Family");
-addToDoItem("Packing", "Weekly Chores", "2022-12-22", "Medium", "Travel");
+addToDoItem("zapp", "Weekly Chores", "2027-01-12", "Low", "Family");
+addToDoItem("Packing", "Weekly Chores", "2027-12-22", "Medium", "Travel");
 addToDoItem("Finish Project", "To Do List", "2025-07-08", "High");
 
 
