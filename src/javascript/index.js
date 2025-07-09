b5d0a3717b9460a154d23c3eb2833fb38c7b0ba9
Tiago Sobral <113380453+TiagoSobral@users.noreply.github.com>
2025-07-09 23:57:03 +0100

    (fix) called different to dos to work on sort func.

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -2,9 +2,8 @@ import "../css/style.css"
 import "../css/svgs.css"
 import "../css/active-styling.css"
 
-import { createToDoList, addToDoItem, changeCompletionStatus, removeToDo} from "./to-do-list-action"
+import { createToDoList, addToDoItem } from "./to-do-list-action"
 import { createDOM } from "./DOM-creation.js";
-import { addProjectListener, addTaskListener, listenerProjectExpand , showToDosFromFilter} from "./DOM-logic.js";
 import { allProjects } from "./to-do-list.js";
 
 createToDoList();
@@ -13,7 +12,7 @@ createToDoList("Travel");
 
 addToDoItem("Cleaning", "Weekly Chores", "2022-02-22", "High", "Family");
 addToDoItem("zapp", "Weekly Chores", "2027-01-12", "Low", "Family");
-addToDoItem("Packing", "Weekly Chores", "2027-12-22", "Medium", "Travel");
+addToDoItem("Packing", "Weekly Chores", "2027-12-22", "Medium", "Family");
 addToDoItem("Finish Project", "To Do List", "2025-07-08", "High");
 
 
