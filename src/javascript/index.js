8848c1173427e33bfb468b5c01d60e286a2d1724
Tiago Sobral <113380453+TiagoSobral@users.noreply.github.com>
2025-07-11 00:58:56 +0100

    (update) new calls for debugging.

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -2,21 +2,25 @@ import "../css/style.css"
 import "../css/svgs.css"
 import "../css/active-styling.css"
 
-import { createToDoList, addToDoItem } from "./to-do-list-action"
+import { createToDoList } from "./to-do-list-action"
 import { createDOM } from "./DOM-creation.js";
 import { allProjects } from "./to-do-list.js";
+import { arrayProjectsToDOM } from "./DOM-content.js";
 
 createToDoList();
-createToDoList("Family");
-createToDoList("Travel");
+// createToDoList("Family");
+// createToDoList("Travel");
 
-addToDoItem("Cleaning", "Weekly Chores", "2022-02-22", "High", "Family");
-addToDoItem("zapp", "Weekly Chores", "2027-01-12", "Low", "Family");
-addToDoItem("Packing", "Weekly Chores", "2027-12-22", "Medium", "Family");
-addToDoItem("Finish Project", "To Do List", "2025-07-08", "High");
+// addToDoItem("Cleaning", "Weekly Chores", "2022-02-22", "High", "Family");
+// addToDoItem("zapp", "Weekly Chores", "2027-01-12", "Low", "Family");
+// addToDoItem("Packing", "Weekly Chores", "2027-12-22", "Medium", "Family");
+// addToDoItem("Finish Project", "To Do List", "2025-07-08", "High");
 
 
 createDOM();
+
+arrayProjectsToDOM();
+
 
 console.log(allProjects);
 
