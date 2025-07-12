01e222c18e7da43f6f1e9156cb73c2dde64ffd21
Tiago Sobral <113380453+TiagoSobral@users.noreply.github.com>
2025-07-12 22:08:11 +0100

    (fix) listeners are now called here.
    (fix) show content as well.

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -4,31 +4,25 @@ import "../css/active-styling.css"
 
 import { createToDoList } from "./to-do-list-action"
 import { createDOM } from "./DOM-creation.js";
-import { allProjects } from "./to-do-list.js";
-import { arrayProjectsToDOM } from "./DOM-content.js";
+import { allProjects, looksForStoredProjects } from "./to-do-list.js";
+import { arrayProjectsToDOM, showProject } from "./DOM-content.js";
+import { filterListener, addProjectListener, addTaskListener } from "./DOM-logic.js";
 
-createToDoList();
-// createToDoList("Family");
-// createToDoList("Travel");
-
-// addToDoItem("Cleaning", "Weekly Chores", "2022-02-22", "High", "Family");
-// addToDoItem("zapp", "Weekly Chores", "2027-01-12", "Low", "Family");
-// addToDoItem("Packing", "Weekly Chores", "2027-12-22", "Medium", "Family");
-// addToDoItem("Finish Project", "To Do List", "2025-07-08", "High");
 
 
 createDOM();
 
+createToDoList();
+
+looksForStoredProjects();
+
 arrayProjectsToDOM();
 
+showProject();
 
-console.log(allProjects);
+filterListener();
 
+addProjectListener();
 
-// listener doesn't work when project expand when a new project is entered.
+addTaskListener();
 
-// today, upcoming and complete btns
-
-// eliminate button for task and project
-
-// figure out what to do with the complete ones 
